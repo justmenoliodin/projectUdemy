6c45e96977c1f32e1ad8be42681bb65e6a584097
Zhansaya <zhansaya.dauletbaeva1229@gmail.com>
2025-04-17 18:28:10 +0500

    first commit

---
+++ PREVIEW/js/script.js
@@ -0,0 +1,12 @@
+"use strict"
+
+
+const answer = [];
+
+answer[0] = prompt('Как ваше имя?', '');
+answer[1] = prompt('Как ваше фамилия?', '');
+answer[2] = prompt('Сколько вам лет?', '');
+
+console.log(answer)
+
+
